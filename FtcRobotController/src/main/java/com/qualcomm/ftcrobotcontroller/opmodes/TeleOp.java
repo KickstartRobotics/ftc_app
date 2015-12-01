42f318032dc3a38d11d20abd641ab330b57bb908
linux-jedi <ankeny901>
2015-12-01 16:38:13 -0600

    Initial Code Commit for teleop mode. This code is based of k9teleop.

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOp.java
@@ -0,0 +1,81 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by kickstart on 11/30/15.
+ */
+public class TeleOp extends OpMode{
+
+    DcMotor motorRight;
+    DcMotor motorLeft;
+
+    public void init()
+    {
+        motorRight = hardwareMap.dcMotor.get("motorRight");
+        motorLeft = hardwareMap.dcMotor.get("motorLeft");
+    }
+
+    public void loop()
+    {
+        // Get values from joysticks
+        float rawRight = gamepad1.right_stick_y;
+        float rawLeft = gamepad1.left_stick_y;
+
+        // clip the right/left values so that the values never exceed +/- 1
+        Float right = Range.clip(rawRight, -1, 1);
+        Float left = Range.clip(rawLeft, -1, 1);
+
+        // scale the joystick value to make it easier to control
+        // the robot more precisely at slower speeds.
+        right = (float) scaleInput(right);
+        left = (float) scaleInput(left);
+
+        // write the values to the motors
+        motorRight.setPower(right);
+        motorLeft.setPower(left);
+    }
+
+
+    public void stop() {
+
+    }
+
+
+    /*
+     * This method scales the joystick input so for low joystick values, the
+     * scaled value is less than linear.  This is to make it easier to drive
+     * the robot more precisely at slower speeds.
+     */
+    double scaleInput(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
+    }
+}
